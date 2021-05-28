e909eb04b71495676df4d0969af9f0db732e6b94
m1chev <veselinov.michev@gmail.com>
2021-05-28 10:53:57 +0300

    adding files in note and updating installHistory

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,14 @@
+alias ll='ls -lha'
+
+# VPN workaround for Fedora
+alias vpn='sudo systemd-resolve -i tun0 --set-dns=10.6.16.100 --set-dns=10.6.16.100 --set-domain=~dc1.vereign.com --set-domain=~tech.vereign.com --set-domain=~k8s.vereign.com --set-domain=~dev.vereign.com'
+
+# VPN Check
+alias vpncheck='firefox https://nexus.tech.vereign.com/'
+
+# Local IP
+alias localip='hostname -I | awk \'{print $1}\''
+
+# Global IP
+alias globalip='curl ipconfig.io'
+
